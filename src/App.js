d2d1fac2c389b7ab2bc56b580d28e709ed3e0627
James Nelson <johndoe@example.com>
2019-11-14 16:53:47 -0700

    did some stylin

--- src/App.js
+++ src/App.js
@@ -1,61 +1,75 @@
-import React from 'react';
-import './App.css';
-import data from './data'; 
-import Name from './Components/Name'
-import Movies from './Components/Movies'
-import Info from './Components/Info'
+import React from "react";
+import "./App.css";
+import data from "./data";
+import Name from "./Components/Name";
+import Movies from "./Components/Movies";
+import Info from "./Components/Info";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       people: data,
       i: 0
-    }
+    };
   }
 
-  handleClick = (e) => {
-    
-    if (e.target.name === 'next') {
-      if(this.state.i === 24){
+  handleClick = e => {
+    if (e.target.name === "next") {
+      if (this.state.i === 24) {
         this.setState({
-          i: this.state.i = 0
-        })
+          i: (this.state.i = 0)
+        });
+      } else {
+        this.setState({
+          i: this.state.i + 1
+        });
       }
-      else{
+    } else if (this.state.i === 0) {
       this.setState({
-        i: this.state.i + 1
-      })
-    }
-    }
-    else if(this.state.i === 0) {
-      this.setState({
-        i: this.state.i = 24
-      })
-    }
-    else{
+        i: (this.state.i = 24)
+      });
+    } else {
       this.setState({
         i: this.state.i - 1
-      })
+      });
     }
-  }
-  render(){
+  };
+  render() {
     return (
-         <div className ="App">
-           {<Name person={this.state.people[this.state.i]}/>}
-           <br/>
-           {<Info person={this.state.people[this.state.i]}/>}
-           <br/>
-           {<Movies person={this.state.people[this.state.i]}/>}
+      <div className="App">
+        <header className="home-header">
+          <div className="home">Home</div>
+        </header>
+        <div className="center">
+          <div className="information-body">
+            <header className="count-header">
+              <div className="counter">{this.state.i + 1}/25</div>
+            </header>
+            {<Name className="name" person={this.state.people[this.state.i]} />}
+            <br />
+            {<Info className="info" person={this.state.people[this.state.i]} />}
+            <br />
+            {
+              <Movies
+                className="Movies"
+                person={this.state.people[this.state.i]}
+              />
+            }
 
-           <div className ='counter'>{this.state.i + 1}/25</div>
-
-           <button name="previous" onClick={this.handleClick}>Previous</button>
-           <button name="next" onClick={this.handleClick}>Next</button>
-        
-
-         </div>
-      
+            <button
+              className="previous"
+              name="previous"
+              onClick={this.handleClick}
+            >
+              Previous
+            </button>
+            <button className="next" name="next" onClick={this.handleClick}>
+              Next
+            </button>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -12,9 +12,9 @@ export default class Movies extends Component{
             <div className="movie-body">
                 <h3>Favorite Movies: </h3>
                 <ol>
-                <li>1. {person.favoriteMovies[0]}</li>
-                <li>2. {person.favoriteMovies[1]}</li>
-                <li>3. {person.favoriteMovies[2]}</li>
+                <li>{person.favoriteMovies[0]}</li>
+                <li>{person.favoriteMovies[1]}</li>
+                <li>{person.favoriteMovies[2]}</li>
                 </ol>
             </div>
         )
